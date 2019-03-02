48805b89f1f977ec5b2dc1dba431acf5c8968d16
IgnacioCorimayo <ignaciocorimayo12@gmail.com>
2019-03-02 07:26:36 -0300

    A

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,32 +1,68 @@
 function mostrar()
 {
-
-   var notaIngresada;
+    var notaIngresada;
     var sexoIngresado;
     var cantidadAlumnos;
+    var sumaNotas;
+    var promedioNotas;
+    var bandera;
+    var cantidadNotasVarones;
 
     cantidadAlumnos = 0;
-    
+    sumaNotas = 0;
+    promedioNotas = 0;
+    bandera = 0;
+    cantidadNotasVarones = 0;
+
     while (cantidadAlumnos < 5)
     {
-    
-    notaIngresada = prompt ("Ingrese una nota");
-    notaIngresada = parseInt (notaIngresada);
+        notaIngresada = prompt ("Ingrese una nota");
+        notaIngresada = parseInt (notaIngresada);
 
-        if (notaIngresada > 0 && notaIngresada < 10)
+        while (notaIngresada < 0 || notaIngresada > 10)
         {
-            sumaNotas = sumaNotas + notaIngresada; 
-            cantidadAlumnos = cantidadAlumnos + 1;
+            notaIngresada = prompt ("Ingrese una nota");
+            notaIngresada = parseInt (notaIngresada);
+        }
+
+        sexoIngresado = prompt ("Ingrese un sexo");
+
+        while (sexoIngresado != "m" && sexoIngresado != "f")
+        {
+            sexoIngresado = prompt ("Ingrese un sexo");
+        }
+
+        if (notaIngresada >= 6 && sexoIngresado == "m")
+        {
+            cantidadNotasVarones = cantidadNotasVarones + 1;
+        }
+
+        sumaNotas = sumaNotas + notaIngresada;
+
+        if (bandera == 0)
+        {
+            notaMinima = notaIngresada;
+            bandera = 1;
         }
         else
-        {
-            notaIngresada = prompt ("Ingrese nuevamente una nota entre 0 y 10"); 
+        {   
+            if (notaIngresada < notaMinima)
+            {
+                notaMinima = notaIngresada;
+                sexoMinimo = sexoIngresado;
+            }
         }
 
-        if (sexoIngresado)
-    sexoIngresado = prompt ("Ingrese un sexo");
-
+        cantidadAlumnos = cantidadAlumnos + 1;
+        
     }
 
+    promedioNotas = sumaNotas / cantidadAlumnos;
+
+    
+    console.log ("La suma de notas es: " +sumaNotas);
+    console.log ("El promedio de notas es: "+promedioNotas);
+    console.log ("La nota más baja es: " +notaMinima+ " y es de sexo: " +sexoMinimo);
+    console.log ("La cantidad de varones con nota mayor o igual a 6 es: " +cantidadNotasVarones);
 
 }
