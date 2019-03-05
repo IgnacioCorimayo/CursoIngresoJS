b1a76d6bda8954251472b0d8e51757019d9ab919
IgnacioCorimayo <ignaciocorimayo12@gmail.com>
2019-03-05 15:31:40 -0300

    M

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/seis.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/seis.js
@@ -1,16 +1,15 @@
-/*Se ingresa una hora. Si está entre las 6 y las 11 mostrar: “es de mañana”, si es desde las 12 a las 19:
+/*Se ingresa una hora. Si está entre las 6 y las 11 mostrar: “es de mañana”,si es desde las 12 a las 19:
 "es de tarde", de lo contrario informar que es de noche. Informar si la hora no es válida.
 Si es de noche y la hora es menor a 24 mostrar se debe agregar el mensaje: "a dormir".*/
 
 function mostrar()
 {
-    var horaIngresada;
+    var hora;
 
-    horaIngresada = document.getElementById ("laHora").value;
-    horaIngresada = parseInt (horaIngresada);
+    hora = document.getElementById ("laHora").value;
+    hora = parseInt (hora);
 
-    
-    switch (horaIngresada)
+    switch  (hora)
     {
         case 6:
         case 7:
@@ -33,19 +32,19 @@ function mostrar()
         break;
 
         default:
-            if (horaIngresada > 20 && horaIngresada < 24 || horaIngresada >= 1 && horaIngresada <= 5)
+            if  (hora >= 20 && hora <= 24 || hora >= 1 && hora <= 5)
             {
                 alert ("Es de noche");
                 
-                if (horaIngresada >5)
+                if  (hora >= 20 && hora <= 24)
                 {   
                     alert ("A dormir");
                 }
-                else
+            }
+            else
                 {
                     alert ("Ingrese una hora válida");   
                 }
-            }
         break;
     
     }
